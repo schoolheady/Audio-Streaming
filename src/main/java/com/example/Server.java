b49f4e8bcefffd9bf874cdf42a703f2159a1f571
David <dj.dorant@student.avans.nl>
2025-10-13 19:54:28 +0700

    Refactor Server class to enhance UDP message handling and implement TCP connection management

--- src/main/java/com/example/Server.java
+++ src/main/java/com/example/Server.java
@@ -1,42 +1,121 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Server{
     private DatagramSocket socket; 
     private byte[] buffer = new byte[256];
+    private Map<String, ClientInfo> clients = new HashMap<>();
 
     public Server(DatagramSocket socket) throws Exception{
         this.socket = socket;
     }
 
+
+    public void startServer() throws Exception{
+        System.out.println("[SERVER] - Server started");
+        // should use TCP to check users that joined the server
+        // check active commands from clients
+        // receive packages from clients and send it to other clients (UDP)
+    }
+
     // function that receives packages from client and sends it to other clients except origin (UDP)
-    public void receiveThenSend() throws Exception{ // repeats message
+    private void receiveThenSend() throws Exception{ // repeats message
         while(true){
-            try {
+
                 DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
-                socket.receive(packet);
-                InetAddress address = packet.getAddress(); 
-                int port = packet.getPort();
                 String messageFromClient = new String(packet.getData(), 0, packet.getLength());
                 System.out.println("[SERVER] - Received message from client: " + messageFromClient);
-                packet = new DatagramPacket(packet.getData(), packet.getLength(), address, port);
 
-                socket.send(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
+                for (ClientInfo c : clients.values()) {
+                    // Assuming you want to forward the received packet to other clients
+                    // We need to create a new packet to send
+                    DatagramPacket sendPacket = new DatagramPacket(packet.getData(), packet.getLength(), c.address, c.udpPort);
+                    socket.send(sendPacket);
+                }
+
         
         }
 
     }
 
     // function that handles commands from client (TCP)
-    public void handleCommands() throws Exception{
+    private void handleCommands() throws Exception{
         // TO-DO
     }
 
+    // heartbeat function to check users in the server (TCP)
+    private void checkUsers() throws Exception{
+        // TO-DO
+    }
+
+    // users need to connect through TCP first before sending/receiving UDP packages
+    private void tcpConnection() throws Exception{
+        Socket socket = null;
+        InputStreamReader input = null;
+        OutputStreamWriter output = null;
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        ServerSocket server = null;
+
+        server = new ServerSocket(4444);
+
+        while(true){
+            try {
+                socket = server.accept(); //blocking
+                clients.put(socket.getInetAddress().toString(), new ClientInfo());
+                
+                input = new InputStreamReader(socket.getInputStream());
+                output = new OutputStreamWriter(socket.getOutputStream());
+                reader = new BufferedReader(input);
+                writer = new BufferedWriter(output);
+
+                String message = reader.readLine();
+                System.out.println("[SERVER] - Received message from client: " + message);
+                writer.write("Message received\n");
+                writer.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;  
+            } finally {
+                try {
+                    if (reader != null) reader.close();
+                    if (writer != null) writer.close();
+                    if (socket != null) socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private void handleAudioStream() throws Exception{
+        // TO-DO
+        int clientid = 0;
+        int sequenceNumber = 0;
+        byte[] audioBuffer = new byte[1024]; 
+
+        // send the audio stream in order of sequence number
+        // TO-DO
+
+
+    }
+
     
+}
+
+class ClientInfo {
+    String id;
+    InetAddress address;
+    int udpPort;
 }
